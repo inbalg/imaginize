637a4fcfade70f3da0d61af89add4f7166f53735
Inbal Gilai <inbal.gilai@klarna.com>
2016-07-31 11:13:54 +0300

    direct root to index

--- config/routes.rb
+++ config/routes.rb
@@ -6,11 +6,12 @@ Rails.application.routes.draw do
   get 'phrases/:id/check_guess' => "phrases#check_guess"
   get 'serve_image/:id' => "phrases#serve_image"
 
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'phrases#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
